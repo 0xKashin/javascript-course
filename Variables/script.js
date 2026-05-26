0abdfad85885e128139b2b562022ffad46210bb5
Chris <olamit.christian143@gmail.com>
2026-05-26 23:31:49 +0800

    Add files via upload

---
+++ Variables/script.js
@@ -0,0 +1,26 @@
+let web;
+let style;
+let program;
+
+let released;
+let declared;
+let published;
+
+let isHard;
+
+web = 'HTML5';
+style = 'CSS3';
+program = 'Javascript';
+
+
+released = 2014;
+declared = 2011;
+published = 1995;
+
+isHard = true;
+
+
+console.log(`${web} was released on ${released}`);
+console.log(`${style} was declared on ${declared}`);
+console.log(`${program} was published on December 4, ${published}`);
+console.log(`${web}, ${style} and, ${program} are the foundation of Frontend Development and that is ${isHard}.`)
