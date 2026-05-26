c835e8487046a39ac63900860027e2f831e56343
Chris <olamit.christian143@gmail.com>
2026-05-26 23:30:57 +0800

    Add files via upload

---
+++ Introduction/script.js
@@ -0,0 +1,11 @@
+// console.log('Hello Javascript, World!');
+// console.log('Javascript is a Programming Language');
+
+// window.alert('Hello Javascript');
+// window.alert('Javascript is a Powerful Programming Language');
+
+// Javascript a Programming Language
+
+document.getElementById('esotericLanguage').textContent = 'HTML';
+document.getElementById('jsDescription').textContent =
+'Hypertext Markup Language is the standard markup language for documents designed to be displayed in a web browser.'
