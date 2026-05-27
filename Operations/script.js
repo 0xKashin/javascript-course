263ffcb011d264b4ad6ccdc8ce95ff8b823e6303
Chris <olamit.christian143@gmail.com>
2026-05-27 13:08:19 +0800

    Add files via upload

---
+++ Operations/script.js
@@ -0,0 +1,20 @@
+let addition = 2;
+let multiply = 4;
+let subtract = 8;
+let division = 10;
+let power = 3;
+let modulo = 11;
+
+addition += 2;
+multiply *= 4;
+subtract -= 4;
+division /= 2;
+power **= 5
+modulo %= 2
+
+console.log(`Addition: ${addition}`)
+console.log(`Multiplication: ${multiply}`)
+console.log(`Subtraction: ${subtract}`)
+console.log(`Division: ${division}`)
+console.log(`Raised to the Power of 3 by 5 is ${power}`);
+console.log(`The remainder of 10 is ${modulo}`);
